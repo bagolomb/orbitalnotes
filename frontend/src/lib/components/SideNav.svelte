ad602d500229e71aa64d60f7bf6e5f2d58d89dd6
Brady Golomb <brgo7557@lasp.colorado.edu>
2025-05-09 09:49:38 -0600

    more

--- frontend/src/lib/components/SideNav.svelte
+++ frontend/src/lib/components/SideNav.svelte
@@ -1,15 +1,16 @@
 <script>
-    import { Menu } from '@lucide/svelte';
+    import { SquareChevronRight, SquareChevronLeft } from '@lucide/svelte';
     let expanded = $state(false);
   </script>
   
   <div class="flex flex-col h-full p-2 border-r-2 {expanded ? 'w-64' : 'w-16'}">
-    <button class="btn btn-soft w-full flex items-center"
-    onclick={() => expanded = !expanded}>
-        <Menu class="flex-none text-primary-text" />
-        <div class="flex w-full h-full justify-center items-center">
+    <button onclick={() => expanded = !expanded} class="btn btn-soft w-full flex p-0">
+        <div class="flex w-full items-center justify-center p-2">
             {#if expanded}
-                <h1>Menu</h1>
+                <SquareChevronLeft class="w-6 h-6 flex-shrink-0"/>
+                <div class="flex-1 text-center">Minimize</div>
+            {:else}
+                <SquareChevronRight class="w-6 h-6 flex-shrink-0"/>
             {/if}
         </div>
     </button>
